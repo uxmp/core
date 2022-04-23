0524626cfb48b0126fa1ad1f51f2448dd1d68c2c
Daniel Jakob <github@usox.org>
2022-04-23 15:01:32 +0200

    Simplify Repository service registration

--- src/Orm/Services.php
+++ src/Orm/Services.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Uxmp\Core\Orm;
 
 use Doctrine\ORM\EntityManagerInterface;
-use Psr\Container\ContainerInterface;
 use Uxmp\Core\Orm\Model\Album;
 use Uxmp\Core\Orm\Model\Artist;
 use Uxmp\Core\Orm\Model\Catalog;
@@ -30,24 +29,15 @@ use Uxmp\Core\Orm\Repository\SongRepositoryInterface;
 use Uxmp\Core\Orm\Repository\UserRepositoryInterface;
 
 return [
-    ArtistRepositoryInterface::class => fn (ContainerInterface $c): ArtistRepositoryInterface =>
-        $c->get(EntityManagerInterface::class)->getRepository(Artist::class),
-    AlbumRepositoryInterface::class => fn (ContainerInterface $c): AlbumRepositoryInterface =>
-        $c->get(EntityManagerInterface::class)->getRepository(Album::class),
-    SongRepositoryInterface::class => fn (ContainerInterface $c): SongRepositoryInterface =>
-        $c->get(EntityManagerInterface::class)->getRepository(Song::class),
-    DiscRepositoryInterface::class => fn (ContainerInterface $c): DiscRepositoryInterface =>
-        $c->get(EntityManagerInterface::class)->getRepository(Disc::class),
-    SessionRepositoryInterface::class => fn (ContainerInterface $c): SessionRepositoryInterface =>
-        $c->get(EntityManagerInterface::class)->getRepository(Session::class),
-    UserRepositoryInterface::class => fn (ContainerInterface $c): UserRepositoryInterface =>
-        $c->get(EntityManagerInterface::class)->getRepository(User::class),
-    CatalogRepositoryInterface::class => fn (ContainerInterface $c): CatalogRepositoryInterface =>
-        $c->get(EntityManagerInterface::class)->getRepository(Catalog::class),
-    FavoriteRepositoryInterface::class => fn (ContainerInterface $c): FavoriteRepositoryInterface =>
-        $c->get(EntityManagerInterface::class)->getRepository(Favorite::class),
-    PlaybackHistoryRepositoryInterface::class => fn (ContainerInterface $c): PlaybackHistoryRepositoryInterface =>
-        $c->get(EntityManagerInterface::class)->getRepository(PlaybackHistory::class),
+    ArtistRepositoryInterface::class => fn (EntityManagerInterface $em) => $em->getRepository(Artist::class),
+    AlbumRepositoryInterface::class => fn (EntityManagerInterface $em) => $em->getRepository(Album::class),
+    SongRepositoryInterface::class => fn (EntityManagerInterface $em) => $em->getRepository(Song::class),
+    DiscRepositoryInterface::class => fn (EntityManagerInterface $em) => $em->getRepository(Disc::class),
+    SessionRepositoryInterface::class => fn (EntityManagerInterface $em) => $em->getRepository(Session::class),
+    UserRepositoryInterface::class => fn (EntityManagerInterface $em) => $em->getRepository(User::class),
+    CatalogRepositoryInterface::class => fn (EntityManagerInterface $em) => $em->getRepository(Catalog::class),
+    FavoriteRepositoryInterface::class => fn (EntityManagerInterface $em) => $em->getRepository(Favorite::class),
+    PlaybackHistoryRepositoryInterface::class => fn (EntityManagerInterface $em) => $em->getRepository(PlaybackHistory::class),
     RadioStationRepositoryInterface::class => fn (EntityManagerInterface $em) => $em->getRepository(RadioStation::class),
     PlaylistRepositoryInterface::class => fn (EntityManagerInterface $em) => $em->getRepository(Playlist::class),
 ];
